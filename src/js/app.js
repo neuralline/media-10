f70bd6e252b1fa10d8f9aa06fbbfb8cd27c83dea
NeuralLine <NeuralDenim>
2019-02-28 13:07:45 +0000

    M10 initial commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import Holo from 'holo-carousel'
+const media10 = () => {
+    Holo.INIT()
+    Holo.AUTO('holo-carousel')
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    media10()
+}, false) //when dom loads do something
